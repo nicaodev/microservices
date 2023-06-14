bf1d0f074203c1e85691d5fd2bbc7b15bfc9c990
nicaodev <alexandre.nicos@gmail.com>
2023-06-14 20:37:39 -0300

    RemoveFromCart

--- GeekShop/GeekShop/GeekShop.CartAPI/Model/Repository/CartRepository.cs
+++ GeekShop/GeekShop/GeekShop.CartAPI/Model/Repository/CartRepository.cs
@@ -45,7 +45,25 @@ public class CartRepository : ICartRepository
 
     public async Task<bool> RemoveFromCart(long cartDetailsId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            CartDetail cartDetail = await _context.CartDetails.FirstOrDefaultAsync(x => x.Id == cartDetailsId);
+
+            int total = _context.CartDetails.Where(x => x.CartHeaderId == cartDetail.CartHeaderId).Count();
+
+            _context.CartDetails.Remove(cartDetail);
+            if (total == 1)
+            {
+                var cartHeaderToRemove = await _context.CartHeaders.FirstOrDefaultAsync(x => x.Id == cartDetail.CartHeaderId);
+                _context.CartHeaders.Remove(cartHeaderToRemove);
+            }
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 
     public async Task<CartDto> SaveOrUpdateCart(CartDto cartDto)
