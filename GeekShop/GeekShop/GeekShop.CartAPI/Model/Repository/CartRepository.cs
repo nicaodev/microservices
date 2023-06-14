bd426a2d42be0b7b7732ab3f3cdd5c9982271022
nicaodev <alexandre.nicos@gmail.com>
2023-06-13 22:19:08 -0300

    def Repository e SaveOrUpdateCart

---
+++ GeekShop/GeekShop/GeekShop.CartAPI/Model/Repository/CartRepository.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using GeekShop.CartAPI.Data.DTOs;
+using GeekShop.CartAPI.Model.Base.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShop.CartAPI.Model.Repository;
+
+public class CartRepository : ICartRepository
+{
+    private readonly MySQLContext _context;
+    private IMapper _mapper;
+
+    public CartRepository(MySQLContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> ApplyCoupon(string userId, string couponCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ClearCart(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> FindCartByUserId(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveCoupon(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveFromCart(long cartDetailsId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> SaveOrUpdateCart(CartDto cartDto)
+    {
+        Cart cart = _mapper.Map<Cart>(cartDto);
+
+        var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == cartDto.CartDetails.FirstOrDefault().ProductId);
+
+        if (product is null)
+        {
+            _context.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+            await _context.SaveChangesAsync();
+        }
+
+        var cartHeader = await _context.CartHeaders.AsNoTracking().FirstOrDefaultAsync(c => c.UserId == cart.CartHeader.UserId);
+
+        if (cartHeader is null)
+        {
+            _context.CartHeaders.Add(cart.CartHeader);
+            await _context.SaveChangesAsync();
+
+            cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.Id;
+            cart.CartDetails.FirstOrDefault().Product = null; // Null pq já foi inserido no contexto o product, acima.
+            _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+
+            await _context.SaveChangesAsync();
+        }
+        else
+        {
+            var cartDetail = await _context.CartDetails.AsNoTracking()
+                .FirstOrDefaultAsync(x => x.ProductId == cart.CartDetails.FirstOrDefault().ProductId && x.CartHeaderId == cartHeader.Id);
+
+            if (cartDetail is null)
+            {
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.Id;
+                cart.CartDetails.FirstOrDefault().Product = null; // Null pq já foi inserido no contexto o product, acima.
+                _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                cart.CartDetails.FirstOrDefault().Product = null;
+                cart.CartDetails.FirstOrDefault().Count += cartDetail.Count;
+                cart.CartDetails.FirstOrDefault().Id = cartDetail.Id;
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cartDetail.CartHeaderId;
+
+                _context.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        return _mapper.Map<CartDto>(cart);
+    }
+}
---
+++ GeekShop/GeekShop/GeekShop.CartAPI/Model/Repository/ICartRepository.cs
@@ -0,0 +1,18 @@
+﻿using GeekShop.CartAPI.Data.DTOs;
+
+namespace GeekShop.CartAPI.Model.Repository;
+
+public interface ICartRepository
+{
+    Task<CartDto> FindCartByUserId(string userId);
+
+    Task<CartDto> SaveOrUpdateCart(CartDto cartDto);
+
+    Task<bool> RemoveFromCart(long cartDetailsId);
+
+    Task<bool> ApplyCoupon(string userId, string couponCode);
+
+    Task<bool> RemoveCoupon(string userId);
+
+    Task<bool> ClearCart(string userId);
+}
